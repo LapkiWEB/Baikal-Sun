24f18047667193e2885d2563091b0081d3668379
Stepan <Stefan.irk96@mail.ru>
2021-11-01 18:06:59 +0800

    add headed

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    $('.menu__btn').on('click', (function () {
+        if (!$('.menu__wrapper').hasClass('openDone')) {
+            $('.menu__wrapper').addClass('openDone');
+        } else {
+            $('.menu__wrapper').removeClass('openDone');
+        }
+        if (!$('.menu__btn').hasClass('active')) {
+            $('.menu__btn').addClass('active');
+        } else {
+            $('.menu__btn').removeClass('active');
+        }
+    }));
+
+
+})
+
